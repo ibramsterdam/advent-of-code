a35291b920e579c88c516ddd757a230e55f2bb71
Bram Janssen <ibramsterdam@hotmail.com>
2022-12-02 21:00:03 +0100

    ADD: day2

--- src/days/day2.ts
+++ src/days/day2.ts
@@ -1,14 +1,38 @@
-export function day2(input) {
-  partOne(input);
+export function day2(file) {
+  partOne(file.split("\n"));
+  partTwo(file.split("\n"));
 }
 
-function partOne(input: number[]) {
-  let result = -1;
+let outcomeScore = {
+  A: { X: 3, Y: 6, Z: 0 },
+  B: { X: 0, Y: 3, Z: 6 },
+  C: { X: 6, Y: 0, Z: 3 },
+};
 
-  console.log(`Result Part 1: ${result}`);
-  partTwo(input);
+let shapeScore = { X: 1, Y: 2, Z: 3 };
+
+function partOne(input: string[]) {
+  let totalScore = 0;
+  for (const round of input) {
+    let [elf, me] = round.split(" ");
+    totalScore += outcomeScore[elf][me] + shapeScore[me];
+  }
+  console.log(`Result Part 2: ${totalScore}`);
 }
 
-function partTwo(input: number[]) {
-  console.log(`Result Part 2: ${input}`);
+let chooseShape = {
+  A: { X: "Z", Y: "X", Z: "Y" },
+  B: { X: "X", Y: "Y", Z: "Z" },
+  C: { X: "Y", Y: "Z", Z: "X" },
+};
+
+function partTwo(input: string[]) {
+  let totalScore = 0;
+
+  for (const round of input) {
+    let [elf, me] = round.split(" ");
+    me = chooseShape[elf][me];
+    totalScore += outcomeScore[elf][me] + shapeScore[me];
+  }
+  console.log(`Result Part 2: ${totalScore}`);
 }
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@ import { join } from "path";
 import { day1 } from "./days/day1";
 import { day2 } from "./days/day2";
 
-const file = readFileSync(join(__dirname, "./input.txt"), "utf-8");
-const input = file
-  .split("\n")
-  .slice(0, -1)
-  .map((n) => Number(n));
+const file = readFileSync(join(__dirname, "./input.txt"), "utf-8").trimEnd();
+// const input = file
+//   .split("\n")
+//   .map((n) => String(n));
 
 // Run day
-day1(input);
-// day2(input);
+// day1(input);
+day2(file);
